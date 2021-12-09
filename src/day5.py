9b235460cd1b21323fab886f145429ee706823dd
Rivo Laks <code@rivolaks.com>
2021-12-09 18:39:03 +0100

    Cleanup

--- src/day5.py
+++ src/day5.py
@@ -1,48 +1,4 @@
 import sys
-from typing import List, Optional, Tuple
-
-
-class MatchException(Exception):
-    pass
-
-
-def get_score(board: List[List[int]], matches: List[List[Optional[int]]], last_drawn: int) -> int:
-    unmarked_nums = []
-    for i in range(5):
-        for j in range(5):
-            if matches[i][j] is None:
-                unmarked_nums.append(board[i][j])
-
-    return sum(unmarked_nums) * last_drawn
-
-
-def play_board(board: List[List[int]], drawn: List[int]) -> Tuple[Optional[int], Optional[int]]:
-    matches = []
-    for i in range(5):
-        matches.append([None] * 5)
-    print(matches)
-
-    for x, num in enumerate(drawn):
-        try:
-            for i, row in enumerate(board):
-                for j, col in enumerate(row):
-                    if col == num:
-                        matches[i][j] = num
-                        # Stupid flow control
-                        raise MatchException()
-        except MatchException:
-            # Check if we've won
-            for i in range(5):
-                if all(matches[i]):
-                    print(num, "row", matches[i], matches)
-                    return x, get_score(board, matches, num)
-            for j in range(5):
-                values = [row[j] for row in matches]
-                if all(values):
-                    print(num, "col", values, matches)
-                    return x, get_score(board, matches, num)
-
-    return None, None
 
 
 def main():
