cd056df42238166b06ec6beb06b923b5def36d3f
Rivo Laks <code@rivolaks.com>
2021-12-20 23:04:12 +0200

    Day 14 part 2 solution

--- src/day14.py
+++ src/day14.py
@@ -1,15 +1,18 @@
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import Dict
 
 
-def run_step(polymer: str, rules: Dict[str, str]) -> str:
-    result = polymer[0]
-    for i in range(1, len(polymer)):
-        insertion = rules.get(polymer[i - 1 : i + 1])
-        if insertion is not None:
-            result += insertion
-        result += polymer[i]
+def run_step(occurrences: Dict[str, int], rules: Dict[str, str]) -> Dict[str, int]:
+    result = defaultdict(int)
+    for pair, count in occurrences.items():
+        insertion = rules.get(pair)
+
+        if insertion is None:
+            result[pair] += count
+        else:
+            result[pair[0] + insertion] += count
+            result[insertion + pair[1]] += count
 
     return result
 
@@ -33,12 +36,26 @@ def main():
     for pattern, insertion in rules:
         print(f"rule: {pattern} -> {insertion}")
 
-    for step in range(10):
-        polymer = run_step(polymer, rules)
-        print(f"after step {step+1}: len={len(polymer)} {polymer=}")
+    # Simulating the whole sequence of letters is prohibitively expensive for 40 steps.
+    # Instead, just keep track of how many of each pairs of letters we have, and simulate those.
+    occurrences = defaultdict(int)
+    # Hack: add special character to the end of the polymer string to make counting easier
+    polymer += "-"
+    for i in range(1, len(polymer)):
+        pair = polymer[i - 1 : i + 1]
+        occurrences[pair] += 1
 
+    for step in range(40):
+        occurrences = run_step(occurrences, rules)
+        print(f"after step {step+1}: len={sum(occurrences.values())} {occurrences=}")
+
+    counter = Counter()
+    for pair, count in occurrences.items():
+        # Avoid double counting by only looking at the first char. The hack above makes it work.
+        counter[pair[0]] += count
+    print(counter)
     # counts is list of  (item, count)  tuples
-    counts = Counter(polymer).most_common()
+    counts = counter.most_common()
 
     print("-" * 50)
     print(counts[0][1] - counts[-1][1])
